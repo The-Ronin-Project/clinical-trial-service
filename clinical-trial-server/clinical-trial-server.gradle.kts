d698620b427d9a0c9dbd526f3fb3d74a2bc455da
douglas-projectronin <123015430+douglas-projectronin@users.noreply.github.com>
2023-10-24 17:48:38 -0400

    CONE-45-establish-authx-for-clinical-trial-service (#5)
    * CONE-45 Created security config for utilizing oauth
    
    * CONE-45 Removed wildcard import

--- clinical-trial-server/clinical-trial-server.gradle.kts
+++ clinical-trial-server/clinical-trial-server.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
     implementation(libs.mysql.connector.java)
     implementation(libs.kotlinx.coroutines.core)
