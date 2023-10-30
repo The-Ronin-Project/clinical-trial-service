edf34bcad4903d4b77b803a959d0b90f4ab4771f
Tripp Whaley <twhaley215@gmail.com>
2023-10-30 18:01:01 -0400

    CONE-56: declarative deployments actions (#10)
    * remove actions yamls and use declarative deployments approach

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #clinical-trial-service
+
+This repository utilizes declarative deployments, see https://projectronin.atlassian.net/wiki/spaces/ENG/pages/2085847083/Migrating+to+Declarative+Deployments+Guide for more details.
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     alias(libs.plugins.interop.server.publish) apply false
     alias(libs.plugins.interop.server.version)
     alias(libs.plugins.interop.version.catalog)
+    alias(libs.plugins.interop.sonarqube)
 
     // We need to force IntelliJ to do some actions they expose through this plugin.
     alias(libs.plugins.idea.ext)
